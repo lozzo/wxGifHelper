bfca073d8fb39db2d7f9f0900f038b7d6bd0f157
lozzo <wuyouget@gmail.com>
2018-12-18 14:29:50 +0800

    添加数据库表

---
+++ init.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS `gifs`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `wxUsers`;
+DROP TABLE IF EXISTS `tgUsers`;
+DROP TABLE IF EXISTS `gifGroups`;
+
+
+CREATE TABLE `gifs`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `GroupID` INTEGER NULL DEFAULT NULL,
+    `FileID` CHAR(40) NOT NULL,
+    `UserID` INTEGER NOT NULL,
+    PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `wxUsers`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `openID` CHAR(40) NOT NULL,
+    `nickName` CHAR(40) NOT NULL DEFAULT "",
+    PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `tgUsers`
+(
+    `id` INTEGER NOT NULL,
+    PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `users`
+(
+    `id` INTEGER NOT NULL,
+    `tgUserID` INTEGER NOT NULL,
+    `wxUserID` INTEGER NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
+
+
+CREATE TABLE `gifGroups`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `groupName` CHAR(40) NOT NULL DEFAULT "",
+    PRIMARY KEY (`id`)
+)
+
+ALTER TABLE `gifs` ADD FOREIGN KEY (GroupID) REFERENCES `gifGroups` (`id`);
+ALTER TABLE `gifs` ADD FOREIGN KEY (UserID) REFERENCES `users` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (tgUserID) REFERENCES `tgUsers` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (wxUserID) REFERENCES `wxUsers` (`id`);
+
+
+
+
+
