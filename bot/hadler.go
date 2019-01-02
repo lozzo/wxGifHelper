0defae1eb479c16911dc446e0fbe3661d3192f30
lozzo <wuyouget@gmail.com>
2019-01-02 09:54:50 +0800

    修改帮助提示，小程序二维码

--- bot/hadler.go
+++ bot/hadler.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	helpStr = `wxGifHelper 是一个帮助将Telegram表情包发送至微信的小工具，配合微信小程序使用，小程序扫描二维码，或者搜索小程序处{}即可添加小程序需要绑定微信号才能使用，绑定后即可使用以下命令，你的绑定号是%d
+	helpStr = `wxGifHelper 是一个帮助将Telegram表情包发送至微信的小工具，配合微信小程序使用，小程序扫描二维码，或者搜索小程序处搜索：表情转发助手 即可添加小程序需要绑定微信号才能使用，绑定后即可使用以下命令，你的绑定号是: %d
 
 			可用命令包括以下：
 
@@ -25,12 +25,12 @@ var (
 			`
 	bindWxStr      = "绑定微信,请在微信小程序设置页面绑定：%d ID,扫描二维码或者搜索小程序:wxGifHelper"
 	bindWxErrStr   = "你已绑定微信，微信昵称：%s，请勿重复绑定，或者解除绑定后重新绑定"
-	unbindWxStr    = "当前绑定微信昵称：%s（昵称更新可能有有延迟）"
+	unbindWxStr    = "当前绑定微信昵称：%s（昵称更新可能有有延迟），解除绑定后仍可以查看已发送的表情。"
 	unbindWxErrStr = "你尚未绑定微信"
 	startSendStr   = "请开始发送表情，结束发送后请输入结束命令：/stop 本次发送状态保持时长为4小时，发送新表情重新计算时长，超过时长自动结束"
 	commandErrStr  = "当前状态不可接受 %s 命令。可接受命令为 %s"
 	notBindWxStr   = "当前尚未绑定微信，无法使用，请在小程序设置页面绑定Telegram ID:%d"
-	wxAppQR        = "" // 微信小程序app二维码
+	wxAppQR        = "AgADBQADZagxG86AYFXmw__s7uQTQXtM9jIABGptsnsGFgNgNjkAAgI" // 微信小程序app二维码
 )
 var cache = common.Cache{
 	Users: make(map[int]*common.MsgStatus),
@@ -68,7 +68,7 @@ func (m *Msg) Handler() {
 		case 0:
 			SendText(m.Message, fmt.Sprintf("收到 %d 个表情", lenth))
 		case 1:
-			SendText(m.Message, "表情已存在，请勿重复发送")
+			SendText(m.Message, "表情本次已存在，请勿重复发送")
 		case 2:
 			SendText(m.Message, "请发送 /send 命令后再开始发送表情")
 		}
@@ -105,8 +105,8 @@ func commndHandler(m *Msg, userMsgStatus *common.MsgStatus) {
 			SendText(m.Message, fmt.Sprintf(bindWxErrStr, name))
 			return
 		}
-		SendText(m.Message, fmt.Sprintf(bindWxStr, m.Message.From.ID))
 		SendImage(m.Message, wxAppQR)
+		SendText(m.Message, fmt.Sprintf(bindWxStr, m.Message.From.ID))
 
 		return
 	case "/un_bind_wx":
@@ -124,7 +124,7 @@ func commndHandler(m *Msg, userMsgStatus *common.MsgStatus) {
 			SendText(m.Message, "已发送0个表情。哟")
 			return
 		}
-		stopSendStr := fmt.Sprintf("已发送 %d 个表情 ,请在用户 %s 用户内打开小程序查看", len(userMsgStatus.File), name)
+		stopSendStr := fmt.Sprintf("已发送 %d 个表情 ,等待后台处理", len(userMsgStatus.File))
 		SendText(m.Message, stopSendStr)
 		return
 	}
